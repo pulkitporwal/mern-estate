a2f8f4710fa60705be3ee9eb773e27fc2de7f346
pulkitporwal <pulkitporwal.brn@gmail.com>
2024-02-04 21:19:06 +0530

    Database Connection has been setuped

---
+++ server/config/db.config.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export default async function(){
+    try {
+        const databaseInstance = await mongoose.connect(
+          `${process.env.MONGODB_URI}/mern-estate`
+        );
+        console.log(
+          "Databse Connected to Host",
+          `HostName: ${databaseInstance.connection.host}`
+        );
+        return databaseInstance;
+      } catch (error) {
+        console.error("ERROR: Issues while connecting to Database", error);
+      }
+}
--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectToDB from "./config/db.config.js";
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Namaste!")
+connectToDB().then(() => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(
+      `Server is running at http://localhost:${process.env.PORT || 3000}`
+    );
+  });
 })
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(
-    `Server is running at http://localhost:${process.env.PORT || 3000}`
-  );
-});
+.catch((err)=>{
+  console.error("MongoDB is Failed to connect with app",err);
+})
