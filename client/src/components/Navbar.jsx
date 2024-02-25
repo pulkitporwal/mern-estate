4ca98cf371ced4087c218c0af68506cfc13924a6
pulkitporwal <pulkitporwal.brn@gmail.com>
2024-02-26 00:00:43 +0530

    Navbar Search Functionality for frontend side has been completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+	const [searchTerm, setsearchTerm] = useState("");
+	const navigate = useNavigate();
+
 	const userInfo = useSelector((state) => state.user) || null;
+
+	const handleSearchSubmit = (e) => {
+		e.preventDefault();
+		const urlParams = new URLSearchParams(window.location.search);
+		urlParams.set("searchTerm", searchTerm);
+		const searchQuery = urlParams.toString();
+
+		navigate(`/search?${searchQuery}`);
+	};
+
+	useEffect(() => {
+		const urlParams = new URLSearchParams(location.search);
+		const searchTermFromURL = urlParams.get("searchTerm");
+		if (searchTermFromURL) {
+			setsearchTerm(searchTermFromURL);
+		}
+	}, [location.search]);
+
 	return (
 		<div className="px-1 sm:px-10 bg-red-100">
 			<div className="flex items-center justify-between p-3 px-5 sm:px-10  border-b-[2px] border-gray-200">
@@ -14,13 +36,20 @@ const Navbar = () => {
 					</h1>
 				</Link>
 
-				<form className="sm:w-[300px] bg-slate-100 rounded-lg flex sm:ml-20 ml-0 px-2 py-2 items-center justify-between">
+				<form
+					onSubmit={handleSearchSubmit}
+					className="sm:w-[300px] bg-slate-100 rounded-lg flex sm:ml-20 ml-0 px-2 py-2 items-center justify-between"
+				>
 					<input
 						type="text"
 						className="w-[100px] bg-transparent focus:outline-none"
 						placeholder="Search..."
+						value={searchTerm}
+						onChange={(e) => setsearchTerm(e.target.value)}
 					/>
-					<CiSearch />
+					<button>
+						<CiSearch />
+					</button>
 				</form>
 
 				<ul className="flex items-center justify-between gap-10 cursor-pointer">
